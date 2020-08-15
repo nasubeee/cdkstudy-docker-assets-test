9ccfada0cda16365ae688580559c820ac31cf513
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2020-08-15 19:12:30 +0900

    Update to cdk 1.59.0. Install cdk modules.

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkDockerAssetsTestStack } from '../lib/cdk-docker-assets-test-stack';
+import { CdkDockerAssetsTestStack } from '../lib/stack';
 
 const app = new cdk.App();
 new CdkDockerAssetsTestStack(app, 'CdkDockerAssetsTestStack');
