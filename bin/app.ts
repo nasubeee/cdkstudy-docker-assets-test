b3ca19ab7346ad5cc1d04ac4dc7c12657ca6522f
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2020-08-15 20:14:07 +0900

    Add resource-name definition module.

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,35 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { ResourceName } from '../lib/resource_name';
 import { CdkDockerAssetsTestStack } from '../lib/stack';
+import { prependOnceListener } from 'process';
 
 const app = new cdk.App();
-new CdkDockerAssetsTestStack(app, 'CdkDockerAssetsTestStack');
+
+//==============================================================================
+// Get Context and define stack env
+//==============================================================================
+const systemName = app.node.tryGetContext("system_name");
+const systemEnv = app.node.tryGetContext("env");
+const resourceName = new ResourceName(systemName, systemEnv);
+const stackEnv = {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+};
+//==============================================================================
+// Get Context and define stack env end
+//==============================================================================
+
+//==============================================================================
+// generate stack
+//==============================================================================
+const stack = new CdkDockerAssetsTestStack(app, 'docker-assets', {
+    stackName: resourceName.stackName(),
+    description: `${resourceName.systemName}`,
+    env: stackEnv,
+    resourceName: resourceName
+});
+//==============================================================================
+// generate stack end
+//==============================================================================
--- lib/stack.ts
+++ lib/stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import ecr = require('@aws-cdk/aws-ecr');
+import { ResourceName } from './resource_name';
+
+export interface CdkDockerAssetsTestStackProps extends cdk.StackProps {
+    resourceName: ResourceName;
+}
 
 export class CdkDockerAssetsTestStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props: CdkDockerAssetsTestStackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // The code that defines your stack goes here
+    }
 }
