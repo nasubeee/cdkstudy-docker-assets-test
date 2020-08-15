7b344c091c9f08d84ef40d7b1868f12a390dc8bf
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2020-08-15 20:46:50 +0900

    Add ecr repository and store uri to the ssm parameter (repository/uri).

--- lib/repository.ts
+++ lib/repository.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import ecr = require('@aws-cdk/aws-ecr');
+import ssm = require('@aws-cdk/aws-ssm');
 import { ResourceName } from './resource_name';
 
 export interface RepositoryStackProps extends cdk.StackProps {
@@ -7,9 +8,21 @@ export interface RepositoryStackProps extends cdk.StackProps {
 }
 
 export class RepositoryStack extends cdk.Stack {
+    public readonly repos: ecr.Repository;
+    public readonly reposUri: ssm.StringParameter;
+
     constructor(scope: cdk.Construct, id: string, props: RepositoryStackProps) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        this.repos = new ecr.Repository(this, `repository`, {
+            repositoryName: props.resourceName.ecrRepositoryName(`assets`),
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+        this.reposUri = new ssm.StringParameter(this, `uri`, {
+            parameterName: props.resourceName.ssmParamName(`repository/uri`),
+            description: `${props.resourceName.systemName} ecr repository uri`,
+            type: ssm.ParameterType.STRING,
+            stringValue: this.repos.repositoryUri 
+        });
     }
 }
